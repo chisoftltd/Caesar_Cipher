e77396e00915f0c70d426cb833a9a2ad7dec330c
Benjamin Chinwe <chisoftltd@gmail.com>
2021-11-09 15:43:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+
+def Caesar_Cipher(plain_text, shift_amount, cipher_direction):
+	cipher_text = ""
+	for letter in plain_text:
+		position = alphabet.index(letter)
+		if cipher_direction == 'encode':
+			new_position = position + shift_amount
+			cipher_text += alphabet[new_position]
+		elif cipher_direction == 'decode':
+			new_position = position - shift_amount
+			cipher_text += alphabet[new_position]
+		else:
+			print("You have choosen wrong direction - encode or decode")
+	print(f"The {cipher_direction} text is \'{cipher_text}\'")
+
+
+Caesar_Cipher(plain_text=text, shift_amount=shift, cipher_direction=direction)
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
