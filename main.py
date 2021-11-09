4903d314cd83c6ece9665b7bc0ee47c0c68bb0d8
Benjamin Chinwe <chisoftltd@gmail.com>
2021-11-09 16:04:11 +0000

    CIPHER - ASCII ART

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+ciper = '''
+        d8b        888                     
+        Y8P        888                     
+                   888                     
+ .d8888b88888888b. 88888b.  .d88b. 888d888 
+d88P"   888888 "88b888 "88bd8P  Y8b888P"   
+888     888888  888888  88888888888888     
+Y88b.   888888 d88P888  888Y8b.    888     
+ "Y8888P88888888P" 888  888 "Y8888 888     
+           888                             
+           888                             
+           888                   
+'''
+print(ciper)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
